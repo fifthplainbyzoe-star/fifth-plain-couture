9918220c782d73e3ce62368542ab1df34cc6056a
fifthplainbyzoe-star <fifthplainbyzoe@gmail.com>
2026-09-09 13:22:17 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import heroImg from "@/assets/select-hero.png";
+import heroImg from "@/assets/hero.png";
 import medallionImg from "@/assets/medallion.jpg";
 import fragranceImg from "@/assets/fragrance.jpg";
 import lookbookImg from "@/assets/lookbook.jpg";
